5db16e9ee5fae4c12cf9a685b857d13b4e6ab974
YuukiTS01 <93564033+YuukiTS01@users.noreply.github.com>
2022-07-04 19:55:29 +0700

    update proc + database

--- DB/19120639_function.sql
+++ DB/19120639_function.sql
@@ -11,3 +11,16 @@ begin
 	set i_MaHD = concat('HD', i_MaHD);
 	return i_MaHD;
 end $$
+
+drop function if exists f_AutoMaVX;
+delimiter $$
+create function f_AutoMaVX() returns char(10)
+begin
+	declare i_MaVX varchar(10) default '00000001';
+	while exists (SELECT * FROM Vaccine WHERE MaVX = concat('VX', i_MaVX)) do
+		SET i_MaVX = i_MaVX + 1;
+		SET i_MaVX = concat(repeat('0',8-LENGTH(convert(i_MaVX,char))), convert(i_MaVX,char));
+	end while;
+	set i_MaVX = concat('VX', i_MaVX);
+	return i_MaVX;
+end $$
