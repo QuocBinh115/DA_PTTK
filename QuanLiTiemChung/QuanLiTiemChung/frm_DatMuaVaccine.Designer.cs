d1720118298e04fb01b308a7672921cedbf1f7fc
Mean-Sun <tranminhsonyi@gmail.com>
2022-07-04 15:16:22 +0700

    update frm_dat mua

--- QuanLiTiemChung/QuanLiTiemChung/frm_DatMuaVaccine.Designer.cs
+++ QuanLiTiemChung/QuanLiTiemChung/frm_DatMuaVaccine.Designer.cs
@@ -31,16 +31,19 @@ namespace QuanLiTiemChung
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.chonvaccine_lable = new System.Windows.Forms.Label();
             this.FormName_label = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.ThemVaccine_btn = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.Soluong_lable = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(102, 247);
+            this.comboBox1.Location = new System.Drawing.Point(96, 154);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(313, 24);
             this.comboBox1.TabIndex = 0;
@@ -48,20 +51,20 @@ namespace QuanLiTiemChung
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(152, 227);
+            this.label1.Location = new System.Drawing.Point(146, 134);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 17);
             this.label1.TabIndex = 1;
             // 
-            // label2
+            // chonvaccine_lable
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(97, 219);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(121, 25);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Chọn vaccin";
+            this.chonvaccine_lable.AutoSize = true;
+            this.chonvaccine_lable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chonvaccine_lable.Location = new System.Drawing.Point(91, 126);
+            this.chonvaccine_lable.Name = "chonvaccine_lable";
+            this.chonvaccine_lable.Size = new System.Drawing.Size(121, 25);
+            this.chonvaccine_lable.TabIndex = 2;
+            this.chonvaccine_lable.Text = "Chọn vaccin";
             // 
             // FormName_label
             // 
@@ -81,29 +84,48 @@ namespace QuanLiTiemChung
             this.label3.Size = new System.Drawing.Size(840, 2);
             this.label3.TabIndex = 4;
             // 
-            // button1
+            // ThemVaccine_btn
             // 
-            this.button1.Location = new System.Drawing.Point(917, 227);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 44);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Thêm vaccine";
-            this.button1.UseVisualStyleBackColor = true;
+            this.ThemVaccine_btn.Location = new System.Drawing.Point(911, 134);
+            this.ThemVaccine_btn.Name = "ThemVaccine_btn";
+            this.ThemVaccine_btn.Size = new System.Drawing.Size(160, 44);
+            this.ThemVaccine_btn.TabIndex = 5;
+            this.ThemVaccine_btn.Text = "Thêm vaccine";
+            this.ThemVaccine_btn.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(543, 158);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // Soluong_lable
+            // 
+            this.Soluong_lable.AutoSize = true;
+            this.Soluong_lable.Location = new System.Drawing.Point(539, 133);
+            this.Soluong_lable.Name = "Soluong_lable";
+            this.Soluong_lable.Size = new System.Drawing.Size(64, 17);
+            this.Soluong_lable.TabIndex = 7;
+            this.Soluong_lable.Text = "Số lượng";
             // 
             // frm_DatMuaVaccine
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1213, 543);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Soluong_lable);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.ThemVaccine_btn);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.FormName_label);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chonvaccine_lable);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
             this.Name = "frm_DatMuaVaccine";
             this.Text = "frm_DatMuaVaccine";
             this.Load += new System.EventHandler(this.frm_DatMuaVaccine_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,9 +135,11 @@ namespace QuanLiTiemChung
 
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label chonvaccine_lable;
         private System.Windows.Forms.Label FormName_label;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ThemVaccine_btn;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label Soluong_lable;
     }
 }
