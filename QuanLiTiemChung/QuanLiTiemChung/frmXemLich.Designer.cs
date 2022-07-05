6db73777986863543890f680d792837aca7f71cc
QuocBinh115 <lqbinh.code@gmail.com>
2022-07-05 17:42:15 +0700

    cmt

--- QuanLiTiemChung/QuanLiTiemChung/frmXemLich.Designer.cs
+++ QuanLiTiemChung/QuanLiTiemChung/frmXemLich.Designer.cs
@@ -29,65 +29,223 @@ namespace QuanLiTiemChung
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmXemLich));
             this.gvLichLamViec = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
             this.cb_MaNV = new System.Windows.Forms.ComboBox();
+            this.bt_huy = new System.Windows.Forms.Button();
+            this.lb_Title = new System.Windows.Forms.Label();
+            this.panel_main = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lb_thanhtoandonhang = new System.Windows.Forms.Label();
+            this.bt_DangXuat = new System.Windows.Forms.Button();
+            this.panel_toolbar = new System.Windows.Forms.Panel();
+            this.panel_User = new System.Windows.Forms.Panel();
+            this.pic_User = new System.Windows.Forms.PictureBox();
+            this.lb_User = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.gvLichLamViec)).BeginInit();
+            this.panel_main.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel_toolbar.SuspendLayout();
+            this.panel_User.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_User)).BeginInit();
             this.SuspendLayout();
             // 
             // gvLichLamViec
             // 
             this.gvLichLamViec.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gvLichLamViec.Location = new System.Drawing.Point(8, 163);
-            this.gvLichLamViec.Margin = new System.Windows.Forms.Padding(2);
+            this.gvLichLamViec.Location = new System.Drawing.Point(3, 34);
+            this.gvLichLamViec.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gvLichLamViec.Name = "gvLichLamViec";
             this.gvLichLamViec.RowHeadersWidth = 62;
             this.gvLichLamViec.RowTemplate.Height = 28;
-            this.gvLichLamViec.Size = new System.Drawing.Size(627, 279);
+            this.gvLichLamViec.Size = new System.Drawing.Size(967, 183);
             this.gvLichLamViec.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(173, 26);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(264, 39);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "LỊCH LÀM VIỆC";
             // 
             // cb_MaNV
             // 
+            this.cb_MaNV.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cb_MaNV.FormattingEnabled = true;
-            this.cb_MaNV.Location = new System.Drawing.Point(63, 115);
+            this.cb_MaNV.Location = new System.Drawing.Point(155, 4);
+            this.cb_MaNV.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cb_MaNV.Name = "cb_MaNV";
-            this.cb_MaNV.Size = new System.Drawing.Size(121, 21);
+            this.cb_MaNV.Size = new System.Drawing.Size(186, 24);
             this.cb_MaNV.TabIndex = 2;
             this.cb_MaNV.SelectedIndexChanged += new System.EventHandler(this.cb_MaNV_SelectedIndexChanged);
             // 
+            // bt_huy
+            // 
+            this.bt_huy.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.bt_huy.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_huy.Location = new System.Drawing.Point(815, 289);
+            this.bt_huy.Margin = new System.Windows.Forms.Padding(4);
+            this.bt_huy.Name = "bt_huy";
+            this.bt_huy.Size = new System.Drawing.Size(164, 37);
+            this.bt_huy.TabIndex = 5;
+            this.bt_huy.Text = "Thoát";
+            this.bt_huy.UseVisualStyleBackColor = true;
+            this.bt_huy.Click += new System.EventHandler(this.bt_huy_Click);
+            // 
+            // lb_Title
+            // 
+            this.lb_Title.AutoSize = true;
+            this.lb_Title.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Title.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lb_Title.Location = new System.Drawing.Point(3, 7);
+            this.lb_Title.Name = "lb_Title";
+            this.lb_Title.Size = new System.Drawing.Size(369, 29);
+            this.lb_Title.TabIndex = 1;
+            this.lb_Title.Text = "HỆ THỐNG TIÊM CHỦNG AN BÌNH";
+            // 
+            // panel_main
+            // 
+            this.panel_main.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(247)))), ((int)(((byte)(247)))));
+            this.panel_main.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_main.Controls.Add(this.bt_huy);
+            this.panel_main.Controls.Add(this.panel1);
+            this.panel_main.Controls.Add(this.lb_thanhtoandonhang);
+            this.panel_main.Location = new System.Drawing.Point(12, 66);
+            this.panel_main.Margin = new System.Windows.Forms.Padding(4);
+            this.panel_main.Name = "panel_main";
+            this.panel_main.Size = new System.Drawing.Size(986, 334);
+            this.panel_main.TabIndex = 32;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.cb_MaNV);
+            this.panel1.Controls.Add(this.gvLichLamViec);
+            this.panel1.Location = new System.Drawing.Point(4, 56);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(975, 228);
+            this.panel1.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(-1, 4);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(148, 24);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "MÃ NHÂN VIÊN";
+            // 
+            // lb_thanhtoandonhang
+            // 
+            this.lb_thanhtoandonhang.AutoSize = true;
+            this.lb_thanhtoandonhang.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_thanhtoandonhang.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lb_thanhtoandonhang.Location = new System.Drawing.Point(346, 11);
+            this.lb_thanhtoandonhang.Name = "lb_thanhtoandonhang";
+            this.lb_thanhtoandonhang.Size = new System.Drawing.Size(245, 41);
+            this.lb_thanhtoandonhang.TabIndex = 4;
+            this.lb_thanhtoandonhang.Text = "LỊCH LÀM VIỆC";
+            // 
+            // bt_DangXuat
+            // 
+            this.bt_DangXuat.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.bt_DangXuat.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_DangXuat.Location = new System.Drawing.Point(832, 4);
+            this.bt_DangXuat.Margin = new System.Windows.Forms.Padding(4);
+            this.bt_DangXuat.Name = "bt_DangXuat";
+            this.bt_DangXuat.Size = new System.Drawing.Size(147, 37);
+            this.bt_DangXuat.TabIndex = 1;
+            this.bt_DangXuat.Text = "Đăng xuất";
+            this.bt_DangXuat.UseVisualStyleBackColor = true;
+            this.bt_DangXuat.Click += new System.EventHandler(this.bt_DangXuat_Click);
+            // 
+            // panel_toolbar
+            // 
+            this.panel_toolbar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(247)))), ((int)(((byte)(247)))));
+            this.panel_toolbar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_toolbar.Controls.Add(this.bt_DangXuat);
+            this.panel_toolbar.Controls.Add(this.lb_Title);
+            this.panel_toolbar.Controls.Add(this.panel_User);
+            this.panel_toolbar.Location = new System.Drawing.Point(12, 11);
+            this.panel_toolbar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel_toolbar.Name = "panel_toolbar";
+            this.panel_toolbar.Size = new System.Drawing.Size(987, 48);
+            this.panel_toolbar.TabIndex = 31;
+            // 
+            // panel_User
+            // 
+            this.panel_User.Controls.Add(this.pic_User);
+            this.panel_User.Controls.Add(this.lb_User);
+            this.panel_User.Location = new System.Drawing.Point(621, 4);
+            this.panel_User.Margin = new System.Windows.Forms.Padding(4);
+            this.panel_User.Name = "panel_User";
+            this.panel_User.Size = new System.Drawing.Size(199, 37);
+            this.panel_User.TabIndex = 3;
+            // 
+            // pic_User
+            // 
+            this.pic_User.BackColor = System.Drawing.Color.Transparent;
+            this.pic_User.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pic_User.BackgroundImage")));
+            this.pic_User.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pic_User.Location = new System.Drawing.Point(0, 0);
+            this.pic_User.Margin = new System.Windows.Forms.Padding(4);
+            this.pic_User.Name = "pic_User";
+            this.pic_User.Size = new System.Drawing.Size(47, 37);
+            this.pic_User.TabIndex = 1;
+            this.pic_User.TabStop = false;
+            // 
+            // lb_User
+            // 
+            this.lb_User.AutoSize = true;
+            this.lb_User.BackColor = System.Drawing.Color.Transparent;
+            this.lb_User.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_User.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lb_User.Location = new System.Drawing.Point(53, 6);
+            this.lb_User.Name = "lb_User";
+            this.lb_User.Size = new System.Drawing.Size(103, 24);
+            this.lb_User.TabIndex = 2;
+            this.lb_User.Text = "Username";
+            // 
             // frmXemLich
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.cb_MaNV);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.gvLichLamViec);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1011, 413);
+            this.Controls.Add(this.panel_main);
+            this.Controls.Add(this.panel_toolbar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmXemLich";
-            this.Text = "XemLich";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Xem lịch làm việc";
             this.Load += new System.EventHandler(this.frmXemLich_Load);
             ((System.ComponentModel.ISupportInitialize)(this.gvLichLamViec)).EndInit();
+            this.panel_main.ResumeLayout(false);
+            this.panel_main.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel_toolbar.ResumeLayout(false);
+            this.panel_toolbar.PerformLayout();
+            this.panel_User.ResumeLayout(false);
+            this.panel_User.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_User)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView gvLichLamViec;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cb_MaNV;
+        private System.Windows.Forms.Button bt_huy;
+        private System.Windows.Forms.Label lb_Title;
+        private System.Windows.Forms.Panel panel_main;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lb_thanhtoandonhang;
+        private System.Windows.Forms.Button bt_DangXuat;
+        private System.Windows.Forms.Panel panel_toolbar;
+        private System.Windows.Forms.Panel panel_User;
+        private System.Windows.Forms.PictureBox pic_User;
+        private System.Windows.Forms.Label lb_User;
     }
 }
