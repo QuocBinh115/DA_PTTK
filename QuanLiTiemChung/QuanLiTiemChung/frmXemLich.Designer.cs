4937fb1bd39eecf6ccbab44b3edcffa0d4348700
Kurokid0206 <hungtyvuive@gmail.com>
2022-07-05 16:03:56 +0700

    Lịch rảnh

--- QuanLiTiemChung/QuanLiTiemChung/frmXemLich.Designer.cs
+++ QuanLiTiemChung/QuanLiTiemChung/frmXemLich.Designer.cs
@@ -31,76 +31,53 @@ namespace QuanLiTiemChung
         {
             this.gvLichLamViec = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
-            this.Ngay = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Ca = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ViTri = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.NhanVien = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cb_MaNV = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.gvLichLamViec)).BeginInit();
             this.SuspendLayout();
             // 
             // gvLichLamViec
             // 
             this.gvLichLamViec.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gvLichLamViec.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Ngay,
-            this.Ca,
-            this.ViTri,
-            this.NhanVien});
-            this.gvLichLamViec.Location = new System.Drawing.Point(12, 152);
+            this.gvLichLamViec.Location = new System.Drawing.Point(8, 163);
+            this.gvLichLamViec.Margin = new System.Windows.Forms.Padding(2);
             this.gvLichLamViec.Name = "gvLichLamViec";
             this.gvLichLamViec.RowHeadersWidth = 62;
             this.gvLichLamViec.RowTemplate.Height = 28;
-            this.gvLichLamViec.Size = new System.Drawing.Size(1176, 528);
+            this.gvLichLamViec.Size = new System.Drawing.Size(627, 279);
             this.gvLichLamViec.TabIndex = 0;
-            this.gvLichLamViec.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gvLichLamViec_CellContentClick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(421, 39);
+            this.label1.Location = new System.Drawing.Point(173, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(401, 59);
+            this.label1.Size = new System.Drawing.Size(264, 39);
             this.label1.TabIndex = 1;
             this.label1.Text = "LỊCH LÀM VIỆC";
             // 
-            // Ngay
+            // cb_MaNV
             // 
-            this.Ngay.HeaderText = "Ngày";
-            this.Ngay.MinimumWidth = 8;
-            this.Ngay.Name = "Ngay";
-            this.Ngay.Width = 300;
-            // 
-            // Ca
-            // 
-            this.Ca.HeaderText = "Ca";
-            this.Ca.MinimumWidth = 8;
-            this.Ca.Name = "Ca";
-            this.Ca.Width = 210;
-            // 
-            // ViTri
-            // 
-            this.ViTri.HeaderText = "Vị Trí";
-            this.ViTri.MinimumWidth = 8;
-            this.ViTri.Name = "ViTri";
-            this.ViTri.Width = 300;
-            // 
-            // NhanVien
-            // 
-            this.NhanVien.HeaderText = "Nhân Viên Phụ Trách";
-            this.NhanVien.MinimumWidth = 8;
-            this.NhanVien.Name = "NhanVien";
-            this.NhanVien.Width = 300;
+            this.cb_MaNV.FormattingEnabled = true;
+            this.cb_MaNV.Location = new System.Drawing.Point(63, 115);
+            this.cb_MaNV.Name = "cb_MaNV";
+            this.cb_MaNV.Size = new System.Drawing.Size(121, 21);
+            this.cb_MaNV.TabIndex = 2;
+            this.cb_MaNV.SelectedIndexChanged += new System.EventHandler(this.cb_MaNV_SelectedIndexChanged);
             // 
             // frmXemLich
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cb_MaNV);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.gvLichLamViec);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmXemLich";
             this.Text = "XemLich";
+            this.Load += new System.EventHandler(this.frmXemLich_Load);
             ((System.ComponentModel.ISupportInitialize)(this.gvLichLamViec)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -111,9 +88,6 @@ namespace QuanLiTiemChung
 
         private System.Windows.Forms.DataGridView gvLichLamViec;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Ngay;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Ca;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ViTri;
-        private System.Windows.Forms.DataGridViewTextBoxColumn NhanVien;
+        private System.Windows.Forms.ComboBox cb_MaNV;
     }
 }
