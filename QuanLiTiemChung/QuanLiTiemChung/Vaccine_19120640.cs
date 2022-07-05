2de2f3167ee5ea72adc264d5b39adf7d35310f94
Mean-Sun <tranminhsonyi@gmail.com>
2022-07-05 15:09:34 +0700

    add file and rebuild

---
+++ QuanLiTiemChung/QuanLiTiemChung/Vaccine_19120640.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiTiemChung
+{
+    class Vaccine_19120640
+    {
+        public string MaVX;
+        public string TenVX;
+        public string NSX;
+        public DateTime HSD;
+        Vaccine_DB_19120640 vaccine_DB = new Vaccine_DB_19120640();
+        public static Dictionary<string, string> Lay_thong_tin_Vaccine()
+        {
+            return Vaccine_DB_19120640.getAllvaccine();
+        }
+    }
+}
