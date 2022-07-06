37a5a924417aea4a0f5a133b7e708ef7a5a0a73d
QuocBinh115 <lqbinh.code@gmail.com>
2022-07-06 10:26:43 +0700

    Update Program.cs

--- QuanLiTiemChung/QuanLiTiemChung/Program.cs
+++ QuanLiTiemChung/QuanLiTiemChung/Program.cs
@@ -32,7 +32,7 @@ namespace QuanLiTiemChung
             Console.Read();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmTT1_Main());
+            Application.Run(new frmChonVacXin());
             
         }
 
